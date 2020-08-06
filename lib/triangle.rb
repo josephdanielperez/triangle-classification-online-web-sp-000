0291020a398f4d4380e4c4f9d302870581b2a0d7
Joseph Perez <hello@josephdanielperez.com>
2020-08-06 05:42:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,12 @@ class Triangle
 
 attr_accessor :first_side, :second_side, :third_side, :equilateral, :isosceles, :scalene
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
+  def initialize(:first_side, :second_side, :third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+    @@sides = [first_side, second_side, third_side] 
+  end 
 
   class TriangleError
   end
