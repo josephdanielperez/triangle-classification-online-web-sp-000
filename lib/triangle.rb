0d0c0a87cc1de28088a85e11e8bbb242cd6e41ae
Joseph Perez <hello@josephdanielperez.com>
2020-08-06 05:41:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-attr_accessor :equilateral, :isosceles, :scalene
+attr_accessor :first_side, :second_side, :third_side, :equilateral, :isosceles, :scalene
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
